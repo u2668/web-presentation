dc7791501a8ddac5df7ed0974b6a0a98c3793b61
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-18 16:26:08 +0300

    Improve remote run  script

--- build-image.sh
+++ build-image.sh
@@ -6,15 +6,3 @@ gulp build
 docker build -t u2668/web-presentation .
 docker push u2668/web-presentation
 
-# local
-#docker rm -f web-presentation
-#docker run --name web-presentation -p 80:80 --net go-to-canteen -d u2668/web-presentation
-
-# remote
-#ssh root@46.101.204.43 docker rm -f web-presentation
-#ssh root@46.101.204.43 docker pull u2668/web-presentation
-#ssh root@46.101.204.43 run --name web-presentation -p 80:80 --net go-to-canteen -d u2668/web-presentation
-
-
-# for debug
-#docker exec -it web-presentation /bin/bash
