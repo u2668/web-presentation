b6651d9a10712796178b79ee264f09087c255c0e
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-19 18:37:16 +0300

    Fix car.

--- src/directives/index/cars/car/car.js
+++ src/directives/index/cars/car/car.js
@@ -2,9 +2,11 @@ import angular from "angular";
 import tmpl from "./car.html"; // for browserify-ng-html2js
 import css from "./car.css"; // for browserify-css
 import client from "../../client/client";
+import _ from "lodash";
 
 class Car {
     constructor() {
+        this.passengers = _.uniq(this.passengers);
     }
 }
 
